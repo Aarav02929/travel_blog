cbca5630828de3baf59e983a69317ef08084dd73
Aarav02929 <2929aaravtiwari@gmail.com>
2025-04-14 18:38:02 +0100

    second commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,24 @@
 export default function Navbar() {
   return (
-    <nav className="w-full bg-white shadow-sm mb-5 border-b border-gray-200">
-      <ul className="flex gap-6 md:gap-12 lg:gap-16 xl:gap-20 p-4 max-w-7xl mx-auto justify-center">
-        <li>
-          <a href="/" className="hover:text-gray-600">
+    <nav className="sticky top-0 z-50 w-full bg-white shadow-sm justify-center">
+      <ul className="flex justify-between p-4 max-w-7xl mx-auto">
+        <li className="px-4">
+          <a href="/" className="hover:text-gray-600 text-lg">
             Home
           </a>
         </li>
-        <li>
-          <a href="/destinations" className="hover:text-gray-600">
+        <li className="px-4">
+          <a href="/destinations" className="hover:text-gray-600 text-lg">
             Destinations
           </a>
         </li>
-        <li>
-          <a href="/guides" className="hover:text-gray-600">
+        <li className="px-4">
+          <a href="/guides" className="hover:text-gray-600 text-lg">
             Travel Guides
           </a>
         </li>
-        <li>
-          <a href="/about" className="hover:text-gray-600">
+        <li className="px-4">
+          <a href="/about" className="hover:text-gray-600 text-lg">
             About
           </a>
         </li>
